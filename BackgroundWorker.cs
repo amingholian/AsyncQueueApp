360da725f8e97d2405ec039253d8a3f38549c722
Amin Gholian <amin.gholian@gmail.com>
2026-02-08 16:44:22 -0500

    Initial commit: Asynchronous task in .NET

---
+++ BackgroundWorker.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading.Tasks;
+
+public class BackgroundWorker
+{
+  private readonly TaskQueue _taskQueue;
+
+  public BackgroundWorker(TaskQueue taskQueue)
+  {
+    _taskQueue = taskQueue;
+  }
+
+  public async Task StartProcessing()
+  {
+    Console.WriteLine("BackgroundWorker StartProcessing");
+    while (await _taskQueue.GetReader().WaitToReadAsync())
+    {
+      var task = await _taskQueue.DequeueTask();
+      await task(); // Execute the task
+    }
+  }
+}
